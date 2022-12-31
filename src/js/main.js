fe6d73ee32e9e804805f681cae868446026f47d0
Marcin <marcin.szturminski@gmail.com>
2022-12-31 14:46:42 +0100

    nav mobile almost ready, trying to add background for closed navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,24 @@
+const navMobile = document.querySelector('.nav-mobile')
+const allNavItems = document.querySelectorAll('.nav__link')
 const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
-	// navMobile.classList.toggle('nav-mobile--active')
+	navMobile.classList.toggle('nav-mobile--active')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
-			// navMobile.classList.remove('nav-mobile--active')
+			navMobile.classList.remove('nav-mobile--active')
 			navBtn.classList.remove('is-active')
 		})
 	})
 }
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
 navBtn.addEventListener('click', handleNav)
+handleCurrentYear()
