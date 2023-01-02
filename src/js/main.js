d7b558453773a0e217b1c1e73f5cbf2d097da3f9
Marcin <marcin.szturminski@gmail.com>
2023-01-02 19:47:17 +0100

    changing the file based on what I did in office on Wilczak

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
-const navMobile = document.querySelector('.nav-mobile')
+const mobileMenu = document.querySelector('.nav__box-mobile')
 const allNavItems = document.querySelectorAll('.nav__link')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
-	navMobile.classList.toggle('nav-mobile--active')
+	mobileMenu.classList.toggle('nav__box-mobile--is-active')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
-			navMobile.classList.remove('nav-mobile--active')
+			mobileMenu.classList.remove('nav__box-mobile--is-active')
 			navBtn.classList.remove('is-active')
 		})
 	})
